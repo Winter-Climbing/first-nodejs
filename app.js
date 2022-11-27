42bdca1a9200c3196e8b0c90eff57f2bb23c9b0b
Gose <niabc70@gmail.com>
2022-11-27 13:20:56 +0900

    util 생성 및 적용, git core.autocrlf input error 해결

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+const homePage = require("./routes/home");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(homePage);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Not Found</h1>");
+});
+
+app.listen(5000);
