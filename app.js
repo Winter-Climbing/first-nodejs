17637467829740e97fdec86e70dce7212b53c37a
Gose <niabc70@gmail.com>
2022-11-29 12:28:35 +0900

    Cannot set headers after they are sent to the client에러 해결로 게시글 등록 기능 업데이트

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.set("view engine", "ejs");
 var db;
 
 MongoClient.connect(
-  "mongodb+srv://admin:1q2w3e4r!@cluster0.tb8du5i.mongodb.net/first-weq?retryWrites=true&w=majority",
+  "mongodb+srv://admin:1q2w3e4r!@cluster0.tb8du5i.mongodb.net/firstwep?retryWrites=true&w=majority",
   (err, client) => {
     if (err) return console.log(err);
 
-    db = client.db("first-weq");
+    db = client.db("firstwep");
 
     app.listen(7000, () => {
       console.log("listening on 7000");
@@ -22,14 +22,75 @@ MongoClient.connect(
 );
 
 const homePage = require("./routes/home");
-const freeBoard = require("./routes/freeBoard");
+
+// freeBoard
+
+app.get("/client/freeBoard", (req, res) => {
+  // 디비에 저장된 post라는 collection 안의 모든 데이터를 꺼내주세요
+  db.collection("post")
+    .find()
+    .toArray((err, out) => {
+      console.log(out);
+      res.render("free.ejs", { posts: out });
+    });
+});
+
+// app.get("/client/freeBoard", (req, res, next) => {
+//   res.render("free.ejs");
+//   next();
+// });
+
+app.use("/client/freeBoard/post", (req, res, next) => {
+  res.render("view.ejs");
+  next();
+});
+
+app.use("/client/freeBoard/register", (req, res, next) => {
+  res.render("register.ejs");
+  next();
+});
+
+app.post("/client/freeAdd/", (req, res, next) => {
+  res.render("free.ejs");
+  console.log(req.body);
+  db.collection("counter").findOne({ name: "게시물개수" }, (err, out) => {
+    let totalRegister = out.totalPost;
+
+    db.collection("post").insertOne(
+      {
+        _id: totalRegister + 1,
+        제목: req.body.title1,
+        내용: req.body.content1,
+      },
+      () => {
+        db.collection("counter").updateOne(
+          { name: "게시물개수" },
+          { $inc: { totalPost: 1 } },
+          (err, out) => {
+            if (err) return console.log(err);
+          }
+        );
+      }
+    );
+  });
+  next();
+});
+
+// 여기
+
+app.get("/", (req, res, next) => {
+  res.render("home.ejs");
+});
+
+// dealBoard
+
 const dealBoard = require("./routes/dealBoard");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/client", dealBoard);
-app.use("/client", freeBoard);
+// app.use("/client", freeBoard);
 app.use(homePage);
 
 app.use((req, res, next) => {
--- routes/freeBoard.js
+++ routes/freeBoard.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-
 const rootDir = require("../util/path");
 
 const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var db;
 
 router.get("/freeBoard", (req, res, next) => {
   res.render("free.ejs");
@@ -18,6 +22,30 @@ router.use("/freeBoard/post", (req, res, next) => {
 
 router.use("/freeBoard/register", (req, res, next) => {
   res.render("register.ejs");
+  next();
+});
+
+router.post("/freeAdd/", (req, res, next) => {
+  res.render("free.ejs");
+  db.collection("counter").findOne({ name: "게시물개수" }, (err, out) => {
+    let totalRegister = out.totalPost;
+
+    db.collection("post").insertOne(
+      {
+        _id: totalRegister + 1,
+        제목: req.body.title,
+        내용: req.body.content,
+      },
+      () => {
+        console.log("저장완료");
+        db.collection("counter").updateOne(
+          { name: "게시물개수" },
+          { $inc: { totalPost: 1 } },
+          (err, out) => {}
+        );
+      }
+    );
+  });
   next();
 });
 
